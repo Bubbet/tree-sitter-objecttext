ecbe3d37b37d3e270b7dce9f64f2a8327d2e6f9f
Bubbet <bubbet@bubbet.dev>
2025-09-20 14:45:50 -0600

    broken bare_words

--- grammar.js
+++ grammar.js
@@ -8,103 +8,78 @@
 // @ts-check
 
 const PREC = {
-  call: 5,
-  parenthesis: 4,
-  urinary: 3,
-  multiplicative: 2,
-  additive: 1,
+  assignment: 4,
+  urary: 3,
+  mul: 2,
+  add: 1,
 }
 
 module.exports = grammar({
   name: "objecttext",
   extras: $ => [
-    /[\s]/,
-    $.comment,
+    /\s/,
+    $.comment
   ],
   supertypes: $ => [
     $.expression,
   ],
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => repeat1($._assignment),
-
-    // Periods can start identifiers but only when followed by another char two dots in a row at the start is not allowed.
-    identifier: $ => prec(-1, /\.?[A-Za-z0-9_][A-Za-z0-9_\.]*/),
-    comment: $ => token(
-      choice(seq("//", /.*/), seq("/*", /[^*]*\*+([^/*][^*]*\*+)*/, "/")),
-    ),
-    _path: $ => alias(token.immediate(/\/\.?[A-Za-z0-9_][A-Za-z0-9_\.]*/), $.identifier),
-  
-
-    internal_reference: $ => seq(alias(/&\/?\.?[A-Za-z0-9_][A-Za-z0-9_\.]*/, $.identifier), repeat($._path)),
-    path_reference: $ => seq(/&<[^>]+>/, repeat($._path)),
-    reference: $ => prec.right(choice(
-      $.internal_reference,
-      $.path_reference,
-    )),
-    value: $ => choice(
-      $.bare_word,
-      $.expression,
-      $.string,
-      $.verbatim,
-    ),
-    bare_word: $ => token.immediate(prec(-2, /\s*[^\[{"].*/)),
-    string: $ => choice(
-      '""',
-      seq('"', repeat(token.immediate(choice(/[^\n"]/, /\\"/))), token.immediate('"')),
-    ),
-    verbatim: $ => seq('@"', /[^"]*/, '"'),
-
-    assignment: $ => seq($.identifier, '=', $.value),
+    source_file: $ => repeat1(prec(-1, $._assignment)),
     _assignment: $ => choice(
+      alias($._bare_word_assignment, $.assignment),
       $.assignment,
-      $.group,
+      $.block,
       $.list,
     ),
 
-    group: $ => seq($.identifier, optional(choice('=', seq(':', repeat1($.extension)))), $._group),
-    list: $ => seq($.identifier, optional(choice('=', seq(':', repeat1($.extension)))), $._list),
+    _bare_word_assignment: $ => prec(-1, seq(alias(/\.?[a-zA-Z0-9_][a-zA-Z0-9_\.]*/, $.identifier), '=', alias(/[^\n]+/, $.bare_word))),
+    assignment: $ => prec(PREC.assignment, seq(field("key", $.identifier), "=", field("value", $.value))),
+    list: $ => prec.right(PREC.assignment, seq(field("key", $.identifier), optional(choice('=', seq(':', repeat1($.extension)))), "[", repeat($._list_value), "]")),
+    block: $ => prec.right(PREC.assignment, seq(field("key", $.identifier), optional(choice('=', seq(':', repeat1($.extension)))), "{", repeat($._block_value), "}")),
+    _list_as_value: $ => prec.right(seq(optional(field("key", $.identifier)), optional(choice('=', seq(':', repeat1($.extension)))), "[", repeat($._list_value), "]")),
+    _block_as_value: $ => prec.right(seq(optional(field("key", $.identifier)), optional(choice('=', seq(':', repeat1($.extension)))), "{", repeat($._block_value), "}")),
+    _list_value: $ => choice($.value, $._assignment),
+    _block_value: $ => $._assignment,
 
+    extension: $ => seq(choice(
+      /<[^>]+>(\/\.?[a-zA-Z0-9_][a-zA-Z0-9_\.]*)*/,
+      /\/?\.?[a-zA-Z0-9_][a-zA-Z0-9_\.]*(\/\.?[a-zA-Z0-9_][a-zA-Z0-9_\.]*)*/
+    ), optional(/[;,]/)),
 
-    internal_extension: $ => seq(alias(/\/?\.?[A-Za-z0-9_][A-Za-z0-9_\.]*/, $.identifier), repeat($._path)),
-    path_extension: $ => seq(/<[^>]+>/, repeat($._path)),
-    extension: $ => prec.right(choice(
-      seq($.internal_extension, /[;,]?/),
-      seq($.path_extension, /[;,]?/),
-    )),
+    reference: $ => choice(
+      /&<[^>]+>(\/\.?[a-zA-Z0-9_][a-zA-Z0-9_\.]*)*/,
+      /&\/?\.?[a-zA-Z0-9_][a-zA-Z0-9_\.]*(\/\.?[a-zA-Z0-9_][a-zA-Z0-9_\.]*)*/
+    ),
 
-    _list: $ => seq('[', repeat(seq(choice(
-      $._assignment,
-      $.value,
-      alias(seq(optional(seq(':', repeat1($.extension))), $._group), $.group),
-      alias(seq(optional(seq(':', repeat1($.extension))), $._list), $.list),
-    ), /[;,]?/)), ']'),
-    _group: $ => seq('{', repeat(seq(choice(
-      $._assignment,
-      alias(seq(optional(seq(':', repeat1($.extension))), $._group), $.group),
-      alias(seq(optional(seq(':', repeat1($.extension))), $._list), $.list),
-    ), /[;,]?/)), '}'),
+    number: $ => /\d*\.?\d+[d]?/,
 
     expression: $ => choice(
       $.number,
       $.reference,
+      $.unary_expression,
       $.binary_expression,
-      $.urinary_expression,
-      $.parenthesized_expression,
-      $.function_call,
     ),
-    urinary_expression: $ => prec(PREC.urinary, seq('-', $.expression)),
-    binary_expression: $ => prec.left(choice(
-      prec(PREC.multiplicative, seq($.expression, choice('*', '/'), $.expression)),
-      prec(PREC.additive, seq($.expression, choice('+', '-'), $.expression)),
+    unary_expression: $ => choice(
+      prec.left(PREC.urary, seq('(', $.expression, ')')),
+      prec.left(PREC.urary, seq('-', $.expression)),
+    ),
+    binary_expression: $ => choice(
+      prec.left(PREC.mul, seq(field('left', $.expression), field('operator', choice("*", "/")), field('right', $.expression))),
+      prec.left(PREC.add, seq(field('left', $.expression), field('operator', choice("+", "-")), field('right', $.expression))),
+    ),
+
+    identifier: $ => /\.?[a-zA-Z0-9_][a-zA-Z0-9_\.]*/,
+    value: $ => choice(
+      alias(token(seq('"', /[^"]*/, '"')), $.string),
+      alias(token(seq('@"', /[^"]*/, '"')), $.virbatim),
+      alias($._list_as_value, $.list),
+      alias($._block_as_value, $.block),
+      $.expression
+    ),
+    comment: (_) => token(choice(
+      seq("//", /[^\n]*/),
+      seq("/*", /([^*]|\*+[^/*])*\*+/, "/")
     )),
-    parenthesized_expression: $ => prec(PREC.parenthesis, seq('(', $.expression, ')')),
-    function_call: $ => prec(PREC.call, seq( // TODO make this not include \( in the identifier match
-      field("name", alias(/\.?[A-Za-z0-9_][A-Za-z0-9_\.]*\(/, $.identifier)),
-      commaSep($.expression),
-      ')',
-    )),
-    number: $ => token(prec(10, /-?\d*\.?\d+%?d?/)),
   }
 });
 
