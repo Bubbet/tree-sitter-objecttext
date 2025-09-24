a843a98beb77fdccf93ec3e01e7ad0c34b4b2375
Bubbet <bubbet@bubbet.dev>
2025-09-23 22:36:49 -0600

    before refactor grammar.js

--- grammar.js
+++ grammar.js
@@ -8,11 +8,11 @@
 // @ts-check
 
 const PREC = {
-  assignment: 5,
-  func: 4,
-  unary: 3,
-  mul: 2,
-  add: 1,
+    assignment: 5,
+    func: 4,
+    unary: 3,
+    mul: 2,
+    add: 1,
 }
 const IDENTIFIER = "\\.?[a-zA-Z0-9_][a-zA-Z0-9_\\.]*"
 const PATH_FILE = "<[^>]+>(\/" + IDENTIFIER + "\)*" //"<[^>]+>(\/" + IDENTIFIER + ")*"
@@ -21,115 +21,114 @@ const INTERNAL_IDENTIFIER = "\(" + IDENTIFIER + "|\\^|\(\\.\\.\))"
 const PATH_INTERNAL = LEADING_GROUP + INTERNAL_IDENTIFIER + "\(\\/" + INTERNAL_IDENTIFIER + "\)*" //"(~|\.|(\.\.))?\/?(" + IDENTIFIER + ")|\^(\/(" + IDENTIFIER + ")|\^)*"
 
 module.exports = grammar({
-  name: "objecttext",
-  extras: $ => [
-    /\s/,
-    $.comment
-  ],
-  supertypes: $ => [
-    $.expression,
-  ],
-  externals: $ => [
-    $.bare_string,
-  ],
-  conflicts: $ => [
-    [$.value, $._block_as_value, $._list_as_value],
-    [$.value, $._block_as_value],
-    [$.value, $._list_as_value],
-    [$.value, $._list_value],
-  ],
-  rules: {
-    source_file: $ => repeat1(prec(-1, $._assignment)),
-    _assignment: $ => choice(
-      $.assignment,
-      $.block,
-      $.list,
-    ),
+    name: "objecttext",
+    extras: $ => [
+        /\s/,
+        $.comment
+    ],
+    supertypes: $ => [
+        $.expression,
+    ],
+    conflicts: $ => [
+        [$.value, $._block_as_value, $._list_as_value],
+        [$.value, $._block_as_value],
+        [$.value, $._list_as_value],
+    ],
+    externals: $ => [
+        $.bare_string, $.error_sentinel
+    ],
+    rules: {
+        source_file: $ => repeat1(prec(-1, $._assignment)),
+        _assignment: $ => choice(
+            $.assignment,
+            $.block,
+            $.list,
+        ),
 
-    assignment: $ => prec(PREC.assignment, seq(field("key", $.identifier), "=", field("value", $.value))),
+        assignment: $ => prec(PREC.assignment, seq(field("key", $.identifier), "=", field("value", $.value))),
 
-    list: $ => prec.right(PREC.assignment, seq(field("key", $.identifier), optional(choice('=', seq(':', repeat1($.extension)))), $._list)),
-    block: $ => prec.right(PREC.assignment, seq(field("key", $.identifier), optional(choice('=', seq(':', repeat1($.extension)))), $._block)),
-    _list_as_value: $ => prec.right(seq(optional(field("key", $.identifier)), optional(choice('=', seq(':', repeat1($.extension)))), $._list)),
-    _block_as_value: $ => prec.right(seq(optional(field("key", $.identifier)), optional(choice('=', seq(':', repeat1($.extension)))), $._block)),
-    _list: $ => seq("[", repeat($._list_value), "]"),
-    _block: $ => seq("{", repeat($._block_value), "}"),
-    _list_value: $ => seq(choice($._assignment, $.value), optional(choice(',', ';'))),
-    _block_value: $ => seq($._assignment, optional(choice(',', ';'))),
+        list: $ => prec.right(PREC.assignment, seq(field("key", $.identifier), optional(choice('=', seq(':', repeat1($.extension)))), $._list)),
+        block: $ => prec.right(PREC.assignment, seq(field("key", $.identifier), optional(choice('=', seq(':', repeat1($.extension)))), $._block)),
+        _list_as_value: $ => prec.right(seq(optional(field("key", $.identifier)), optional(choice('=', seq(':', repeat1($.extension)))), $._list)),
+        _block_as_value: $ => prec.right(seq(optional(field("key", $.identifier)), optional(choice('=', seq(':', repeat1($.extension)))), $._block)),
+        _list: $ => seq("[", repeat($._list_value), "]"),
+        _block: $ => seq("{", repeat($._block_value), "}"),
+        _list_value: $ => seq(choice($._assignment, $.value), optional(choice(',', ';'))),
+        _block_value: $ => seq($._assignment, optional(choice(',', ';'))),
 
-    extension: $ => seq(choice(
-      new RegExp(PATH_FILE),
-      new RegExp(PATH_INTERNAL),
-    ), optional(/[;,]/)),
+        extension: $ => seq(choice(
+            new RegExp(PATH_FILE),
+            new RegExp(PATH_INTERNAL),
+        ), optional(/[;,]/)),
 
-    reference: $ => choice(
-      new RegExp("&" + PATH_FILE),
-      new RegExp("&" + PATH_INTERNAL),
-    ),
+        reference: $ => choice(
+            new RegExp("&" + PATH_FILE),
+            new RegExp("&" + PATH_INTERNAL),
+        ),
 
-    number: _ => {
-      const decimalDigits = /\d(_?\d)*/;
-      const signedInteger = seq(optional(choice('-', '+')), decimalDigits);
-      const exponentPart = seq(choice('e', 'E'), signedInteger);
-      const decimalIntegerLiteral = choice(
-        '0',
-        seq(optional('0'), /[1-9]/, optional(seq(optional('_'), decimalDigits))),
-      );
+        number: _ => {
+            const decimalDigits = /\d(_?\d)*/;
+            const signedInteger = seq(optional(choice('-', '+')), decimalDigits);
+            const exponentPart = seq(choice('e', 'E'), signedInteger);
+            const decimalIntegerLiteral = choice(
+                '0',
+                seq(optional('0'), /[1-9]/, optional(seq(optional('_'), decimalDigits))),
+            );
 
-      const decimalLiteral = choice(
-        seq(decimalIntegerLiteral, '.', optional(decimalDigits), optional(exponentPart)),
-        seq('.', decimalDigits, optional(exponentPart)),
-        seq(decimalIntegerLiteral, exponentPart),
-        decimalDigits,
-      );
+            const decimalLiteral = choice(
+                seq(decimalIntegerLiteral, '.', optional(decimalDigits), optional(exponentPart)),
+                seq('.', decimalDigits, optional(exponentPart)),
+                seq(decimalIntegerLiteral, exponentPart),
+                decimalDigits,
+            );
 
-      return token(choice(
-        decimalLiteral,
-        /\d*\.?\d+[dr%]?/,
-      ));
-    },
-    expression: $ => choice(
-      $.number,
-      $.reference,
-      $.unary_expression,
-      $.binary_expression,
-      $.function_call,
-    ),
-    unary_expression: $ => choice(
-      prec.left(PREC.unary, seq('(', $.expression, ')')),
-      prec.left(PREC.unary, seq('-', $.expression)),
-    ),
-    binary_expression: $ => choice(
-      prec.left(PREC.mul, seq(field('left', $.expression), field('operator', choice("*", "/")), field('right', $.expression))),
-      prec.left(PREC.add, seq(field('left', $.expression), field('operator', choice("+", "-")), field('right', $.expression))),
-    ),
-    // TODO this techinically isnt part of ObjectText, it just parses the bare_word as a expression using mxparser
-    // Though, ObjectText does replace references inside that bareword with the real value.
-    function_call: $ => prec.left(PREC.func, seq($.identifier, token.immediate('('), repeat1(seq($.expression, optional(','))), ')')),
+            return token(choice(
+                decimalLiteral,
+                /\d*\.?\d+[dr%]?/,
+            ));
+        },
+        expression: $ => choice(
+            $.number,
+            $.reference,
+            $.unary_expression,
+            $.binary_expression,
+            $.function_call,
+        ),
+        unary_expression: $ => choice(
+            prec.left(PREC.unary, seq('(', $.expression, ')')),
+            prec.left(PREC.unary, seq('-', $.expression)),
+        ),
+        binary_expression: $ => choice(
+            prec.left(PREC.mul, seq(field('left', $.expression), field('operator', choice("*", "/")), field('right', $.expression))),
+            prec.left(PREC.add, seq(field('left', $.expression), field('operator', choice("+", "-")), field('right', $.expression))),
+        ),
+        // TODO this techinically isnt part of ObjectText, it just parses the bare_word as a expression using mxparser
+        // Though, ObjectText does replace references inside that bareword with the real value.
+        function_call: $ => prec.left(PREC.func, seq($.identifier, token.immediate('('), repeat1(seq($.expression, optional(','))), ')')),
 
-    bool: $ => choice('true', 'false'),
-    //bare_string: $ => token(prec(-2, /[^\n]+/)),
-    identifier: $ => token(prec(-2, new RegExp(IDENTIFIER))),
-    value: $ => choice(
-      alias(token(seq('"', /([^"]|(\\"))*/, '"')), $.string),
-      alias(token(seq('@"', /([^"]|(\\"))*/, '"')), $.virbatim),
-      alias($._list_as_value, $.list),
-      alias($._block_as_value, $.block),
-      $.expression,
-      $.identifier,
-      $.bool,
-      $.value,
-    ),
-    comment: (_) => token(choice(
-      seq("//", /[^\n]*/),
-      seq("/*", /([^*]|\*+[^/*])*\*+/, "/")
-    )),
-  }
+        bool: $ => choice('true', 'false'),
+        //bare_string: $ => token(prec(-2, /[^\n]+/)),
+        identifier: $ => token(prec(-2, new RegExp(IDENTIFIER))),
+        value: $ => choice(
+            alias(token(seq('"', /([^"]|(\\"))*/, '"')), $.string),
+            alias(token(seq('@"', /([^"]|(\\"))*/, '"')), $.virbatim),
+            alias($._list_as_value, $.list),
+            alias($._block_as_value, $.block),
+            $.expression,
+            $.identifier,
+            $.bool,
+            prec(10, $.bare_string),
+        ),
+        comment: (_) => token(choice(
+            seq("//", /[^\n]*/),
+            seq("/*", /([^*]|\*+[^/*])*\*+/, "/")
+        )),
+    }
 });
 
 /**
  * @param {RuleOrLiteral} rule
  */
 function commaSep(rule) {
-  return optional(seq(rule, repeat(seq(',', rule))));
+    return optional(seq(rule, repeat(seq(',', rule))));
 }
--- src/scanner.c
+++ src/scanner.c
@@ -2,9 +2,18 @@
 #include <wchar.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 enum {
   BARE_STRING,
+  ERROR_SENTINEL,
+};
+
+enum {
+    NOT_IDENTIFIER,
+    MAYBE_IDENTIFIER,
+    SPACE_AFTER_IDENTIFIER,
+    MORE_AFTER_IDENTIFIER,
 };
 
 void * tree_sitter_objecttext_external_scanner_create() { return NULL; }
@@ -18,42 +27,56 @@ void tree_sitter_objecttext_external_scanner_deserialize(void *payload, const ch
   (void)payload; (void)buffer; (void)length;
 }
 
+static bool is_identifier_char(int32_t c) {
+    return (c >= 'a' && c <= 'z') ||
+         (c >= 'A' && c <= 'Z') ||
+         (c >= '0' && c <= '9') ||
+         c == '_' || c == '.';
+}
+
 bool tree_sitter_objecttext_external_scanner_scan(void *payload, TSLexer *lexer,
                                                   const bool *valid_symbols) {
-  if (!valid_symbols[BARE_STRING]) return false;
-  return false;
+  printf("Found char: %c %d %c\n", lexer->lookahead, lexer->lookahead, valid_symbols[BARE_STRING] ? 'y' : 'n');
+  if (!valid_symbols[BARE_STRING] || valid_symbols[ERROR_SENTINEL]) return false;
 
-  bool maybe_identifier = false;
-  bool only_an_identifier = false;
   bool in_quotes = false;
-  while (!lexer->eof && lexer->lookahead != '\n') {
+  int identifier_state = NOT_IDENTIFIER;
+  bool has_stuff = false;
+  while (!lexer->eof(lexer) && lexer->lookahead != '\n') { //(!lexer->eof && lexer->lookahead != '\n') {
     int32_t lookahead = lexer->lookahead;
-    if (('\t' <= lookahead && lookahead <= '\r') ||
-          lookahead == ' ') {
-      if (maybe_identifier && !only_an_identifier)
-            only_an_identifier = true;
+    printf("lookahead: %c %d\n", lookahead, lookahead);
+    if (('\t' <= lookahead && lookahead <= '\r') || lookahead == ' ') {
+      if (identifier_state == MAYBE_IDENTIFIER) identifier_state = SPACE_AFTER_IDENTIFIER;
+      printf("found space\n");
       lexer->advance(lexer, true);
       continue;
     }
 
     if (!in_quotes) {
-      if (lookahead == "\"") {
+      if (lookahead == '"') {
         in_quotes = true;
         lexer->advance(lexer, false);
+        printf("found quote\n");
         continue;
       }
-      if (lookahead == "[" || lookahead == "{" || lookahead == ":" || lookahead == "&" || lookahead == "\"" || lookahead == ";" || lookahead == ",")
-        return false;
+      if (lookahead == '[' || lookahead == ']' || lookahead == '{' || lookahead == ':' || lookahead == '&' || lookahead == '"' || lookahead == ';' || lookahead == ',') { printf("returning false: terminator char found\n"); return false; }
+      if (identifier_state == NOT_IDENTIFIER && is_identifier_char(lookahead)) identifier_state = MAYBE_IDENTIFIER;
+      if (identifier_state == SPACE_AFTER_IDENTIFIER) identifier_state = MORE_AFTER_IDENTIFIER;
+    } else if (lookahead == '"') {
+      printf("returning false: found string or virbatim\n");
+      return false;
     }
 
-    maybe_identifier = true;
-
-    if (only_an_identifier) only_an_identifier = false;
+    has_stuff = true;
+    lexer->advance(lexer, false);
   }
 
-  if (only_an_identifier) return false;
-  
+  if (!has_stuff) { printf("returning false: empty\n"); return false; }
+
+  if (identifier_state == MAYBE_IDENTIFIER || identifier_state == SPACE_AFTER_IDENTIFIER) { printf("returning false: identifier found\n"); return false; }
+
   lexer->mark_end(lexer);
   lexer->result_symbol = BARE_STRING;
+  printf("returning true\n");
   return true;
 }
