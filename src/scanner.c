f7b303734da895972e80702be3bb8ee7c086bd48
Bubbet <bubbet@bubbet.dev>
2025-09-25 16:05:28 -0600

    Port the C# parser, it has infinite recursion because its not meant to be used like this

--- src/scanner.c
+++ src/scanner.c
@@ -6,97 +6,243 @@
 
 #define SCANNER_LOGGING
 #if defined(__EMSCRIPTEN__) || defined(__wasm__) || !defined(SCANNER_LOGGING)
-  // WASM build: define as no-op
-  #define LOG(...)
+// WASM build: define as no-op
+#define LOG(...)
 #else
-  // Non-WASM build: real definition
-  #define LOG(...) printf(__VA_ARGS__)
+// Non-WASM build: real definition
+#define LOG(...) printf(__VA_ARGS__);
 #endif
 
-#define ADVANCE(...) LOG("Advancing from char: %c %d\n", lexer->lookahead, lexer->lookahead); \
+#define LEXER_ADVANCE(...) LOG("Advancing from c:\"%c\" d:\"%d\" s:\"%d\"\n", lexer->lookahead, lexer->lookahead, lexer_state) \
 lexer->advance(lexer, __VA_ARGS__); \
-LOG("New lookahead %c %d\n", lexer->lookahead, lexer->lookahead);
+LOG("New lookahead c:\"%c\" d:\"%d\"\n", lexer->lookahead, lexer->lookahead)
 
 enum {
-  BARE_STRING,
-  ERROR_SENTINEL,
+    BARE_STRING,
+    ERROR_SENTINEL,
 };
 
+
+// FState
 enum {
-    NOT_IDENTIFIER,
-    MAYBE_IDENTIFIER,
-    SPACE_AFTER_IDENTIFIER,
-    MORE_AFTER_IDENTIFIER,
+    Start,
+    MaybeIdentifierStartsPeriod,
+    Identifier,
+    Whitespace,
+    Slash,
+    MaybeCommentStart,
+    LineComment,
+    BlockComment,
+    BlockCommentMaybeEnd,
+    Quote,
+    QuoteBackslash,
+    VerbatimQuoteMaybeStartOrEnd,
+    VerbatimQuote,
+    AddToken,
+    Error,
 };
 
-void * tree_sitter_objecttext_external_scanner_create() { return NULL; }
-void tree_sitter_objecttext_external_scanner_destroy(void *payload) { (void)payload; }
+void *tree_sitter_objecttext_external_scanner_create() { return NULL; }
+void tree_sitter_objecttext_external_scanner_destroy(void *payload) { (void) payload; }
 
 unsigned tree_sitter_objecttext_external_scanner_serialize(void *payload, char *buffer) {
-  (void)payload; (void)buffer;
-  return 0;
+    (void) payload;
+    (void) buffer;
+    return 0;
 }
+
 void tree_sitter_objecttext_external_scanner_deserialize(void *payload, const char *buffer, unsigned length) {
-  (void)payload; (void)buffer; (void)length;
+    (void) payload;
+    (void) buffer;
+    (void) length;
 }
 
 static bool is_identifier_char(const int32_t c) {
     return (c >= 'a' && c <= 'z') ||
-         (c >= 'A' && c <= 'Z') ||
-         (c >= '0' && c <= '9') ||
-         c == '_' || c == '.';
+           (c >= 'A' && c <= 'Z') ||
+           (c >= '0' && c <= '9') ||
+           c == '_' || c == '.';
 }
 
 static bool is_terminator_char(const int32_t c) {
-  return c == '[' || c == ']' || c == '{' || c == '}' || c == ':' || c == '&' || c == '"' || c == ';' || c == ',';
+    return c == '[' || c == ']' || c == '{' || c == '}' || c == ':' || c == '&' || c == '"' || c == ';' || c == ',';
+}
+
+static bool is_whitespace_char(const int32_t c) {
+    return c == '\t' || c == ' ';
+}
+
+static bool is_line_break_char(const int32_t c) {
+    return c == '\n' || c == '\r';
+}
+
+static bool is_whitespace(const int32_t c) {
+    return c == 9 || c == 10 || c == 13 || c == 32 || c == 92;
+}
+
+static bool can_start_identifier(const int32_t c) {
+    return c != '.' && is_identifier_char(c);
 }
 
 bool tree_sitter_objecttext_external_scanner_scan(void *payload, TSLexer *lexer,
                                                   const bool *valid_symbols) {
-  char error = valid_symbols[ERROR_SENTINEL] ? 'y' : 'n';
-  LOG("Found char: %c %d %c\n", lexer->lookahead, lexer->lookahead, error);
-  if (!valid_symbols[BARE_STRING] || valid_symbols[ERROR_SENTINEL]) return false;
+    if (!valid_symbols[BARE_STRING] || valid_symbols[ERROR_SENTINEL]) return false;
 
-  bool in_quotes = false;
-  int identifier_state = NOT_IDENTIFIER;
-  bool has_stuff = false;
-  while (!lexer->eof(lexer)) {// && lexer->lookahead != '\n') { //(!lexer->eof && lexer->lookahead != '\n') {
-    int32_t lookahead = lexer->lookahead;
-    LOG("lookahead: %c %d %c\n", lookahead, lookahead, error);
-    if (('\t' <= lookahead && lookahead <= '\r' && lookahead != '\n') || lookahead == ' ') {
-      if (identifier_state == MAYBE_IDENTIFIER) identifier_state = SPACE_AFTER_IDENTIFIER;
-      LOG("found space %c\n", error);
-      ADVANCE(true)
-      continue;
+    int lexer_state = Start;
+    bool has_stuff = false;
+    while (!lexer->eof(lexer)) {
+        const int32_t lookahead = lexer->lookahead;
+
+        switch (lexer_state) {
+            case Start:
+                if (can_start_identifier(lookahead)) {
+                    lexer_state = Identifier;
+                    break;
+                }
+                if (lookahead == '.') {
+                    lexer_state = MaybeIdentifierStartsPeriod;
+                    break;
+                }
+                if (is_whitespace(lookahead)) {
+                    lexer_state = Whitespace;
+                    break;
+                }
+                if (lookahead == 47) { // /
+                    lexer_state = Slash;
+                    break;
+                }
+                if (lookahead == 34) { // "
+                    lexer_state = Quote;
+                    break;
+                }
+                if (lookahead == 64) { // @
+                    lexer_state = VerbatimQuoteMaybeStartOrEnd;
+                    break;
+                }
+                if (lookahead >= 0) { // else
+                    lexer_state = AddToken;
+                    break; // TODO maybe break out of loop
+                }
+                break;
+            case MaybeIdentifierStartsPeriod:
+                if (can_start_identifier(lookahead)) {
+                    lexer_state = Identifier;
+                    break;
+                }
+                goto break_loop; // Was GOTO AddToken
+            case Identifier:
+                if (is_identifier_char(lookahead)) {
+                    break;
+                } // TODO dont goto break loop, we need to check for spaces followed by more identifiers AKA lexer_state = Start
+                goto break_loop; // Was GOTO AddToken
+            case Whitespace:
+                if (is_whitespace(lookahead)) break;
+                if (lookahead == 47) { // /
+                    lexer_state = MaybeCommentStart;
+                } // TODO dont goto break loop, we need to check for spaces followed by more identifiers AKA lexer_state = Start
+                goto break_loop; // Was GOTO AddToken
+            // Start Probably Not Needed
+            case Slash:
+                if (lookahead == 42) { // *
+                    lexer_state = BlockComment;
+                    break;
+                }
+                if (lookahead == 47) { // /
+                    lexer_state = LineComment;
+                    break;
+                }
+                goto break_loop; // Was GOTO AddToken
+            case LineComment:
+                if (lookahead == 10) { // \n
+                    lexer_state = Whitespace;
+                    break;
+                }
+                if (lookahead >= 0) { // else
+                    break;
+                }
+                goto break_loop; // Was GOTO AddToken
+            case BlockComment:
+                if (lookahead == 42) { // *
+                    lexer_state = BlockCommentMaybeEnd;
+                    break;
+                }
+                if (lookahead >= 0) { // else
+                    break;
+                }
+                lexer_state = Error;
+                break; // Was GOTO Error
+            case BlockCommentMaybeEnd:
+                if (lookahead == 47) { // /
+                    lexer_state = Whitespace;
+                    break;
+                }
+                if (lookahead >= 0) { // else
+                    lexer_state = BlockComment;
+                    break;
+                }
+                goto break_loop; // Was GOTO AddToken
+            // End Probably Not Needed
+            case Quote:
+                if (lookahead == 34) { // "
+                    lexer_state = AddToken; // TODO this should probably just break out
+                    break;
+                }
+                if (lookahead == 92) { // \ backslash
+                    lexer_state = QuoteBackslash;
+                    break;
+                }
+                if (lookahead >= 0 && lookahead != 10) { // anything but \n
+                    break;
+                }
+                lexer_state = Error;
+                break;
+            case QuoteBackslash:
+                if (lookahead >= 0 && lookahead != 10) { // anything but \n
+                    lexer_state = Quote;
+                    break;
+                }
+                lexer_state = Error;
+                break; // Was GOTO Error
+            case VerbatimQuoteMaybeStartOrEnd:
+                if (lookahead == 34) { // "
+                    lexer_state = VerbatimQuote;
+                    break;
+                }
+                goto break_loop; // Was GOTO AddToken
+            case VerbatimQuote:
+                if (lookahead == 34) { // "
+                    lexer_state = VerbatimQuoteMaybeStartOrEnd;
+                    break;
+                }
+                if (lookahead >= 0) { // else
+                    break;
+                }
+                lexer_state = Error;
+                break; // Was GOTO Error
+            case AddToken:
+                goto break_loop;
+            default:
+                lexer_state = Error;
+                goto break_loop;
+        }
+
+        lexer->mark_end(lexer);
+        has_stuff = true;
+        LEXER_ADVANCE(lexer_state != Whitespace)
     }
 
-    if (!in_quotes) {
-      if (lookahead == '"') {
-        in_quotes = true;
-        ADVANCE(false)
-        LOG("found quote %c\n", error);
-        continue;
-      }
-      if ((identifier_state == NOT_IDENTIFIER || identifier_state == MORE_AFTER_IDENTIFIER) && (lookahead == '\n' || is_terminator_char(lookahead))) break;
-      if (is_terminator_char(lookahead)) { LOG("returning false: terminator char found %c\n", error); return false; }
-      if (identifier_state == NOT_IDENTIFIER && is_identifier_char(lookahead)) identifier_state = MAYBE_IDENTIFIER;
-      if (identifier_state == MAYBE_IDENTIFIER && !is_identifier_char(lookahead)) identifier_state = MORE_AFTER_IDENTIFIER;
-      if (identifier_state == SPACE_AFTER_IDENTIFIER) identifier_state = MORE_AFTER_IDENTIFIER;
-    } else if (lookahead == '"') {
-      LOG("returning false: found string or verbatim %c\n", error);
-      return false;
+break_loop:
+    if (!has_stuff) {
+        LOG("Returning false: empty\n")
+        return false;
     }
 
-    has_stuff = true;
-    ADVANCE(false)
-  }
+    if (lexer_state != AddToken) {
+        LOG("Returning false: invalid state \"%d\"\n", lexer_state)
+        return false;
+    }
 
-  if (!has_stuff) { LOG("returning false: empty %c\n", error); return false; }
-
-  if (identifier_state == MAYBE_IDENTIFIER || identifier_state == SPACE_AFTER_IDENTIFIER) { LOG("returning false: identifier found %c\n", error); return false; }
-
-  lexer->mark_end(lexer);
-  lexer->result_symbol = BARE_STRING;
-  LOG("returning true %c\n", error);
-  return true;
+    lexer->result_symbol = BARE_STRING;
+    LOG("Returning true\n")
+    return true;
 }
